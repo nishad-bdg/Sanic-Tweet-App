c59f2729f8a166ea30a8a5e855a10e33c7cfd294
Nishad Islam <nishadislam@Nishads-MacBook-Pro.local>
2019-04-25 09:25:44 +0600

    error exceptions added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from sanic import Sanic,response
+from sanic import Sanic,response,exceptions
 from sanic.response import json
 from sanic.views import HTTPMethodView
 from sanic_jwt import exceptions,initialize
 from sanic_jwt.decorators import protected
 from sanic_session import Session,InMemorySessionInterface
 from sanic_openapi import swagger_blueprint
+from sanic_cors import CORS, cross_origin
 from db import *
 import hashlib, binascii
 import os
@@ -13,6 +14,10 @@ import os
 
 
 app = Sanic(__name__)
+CORS(app, automatic_options=True)
+
+
+
 session = Session(app, interface=InMemorySessionInterface())
 
 async def authenticate(request, *args, **kwargs):
@@ -25,7 +30,9 @@ async def authenticate(request, *args, **kwargs):
     else:
         try:
             user = User.get(username = username, password = hash_password)
-            request['session']['user'] = user.username
+            #request['session']['user'] = user.username
+            request['session']['user_id'] = user.id
+            
         except:
             raise exceptions.AuthenticationFailed("invalid username or password")
     return {"user_id": user.id}
@@ -40,24 +47,24 @@ class UserRegister(HTTPMethodView):
         message = {}
         username = request.json.get('username',None)
         password = request.json.get('password',None)
+        
         if not username or not password:
-            message['error'] = "username and password required"
-        else:
-            try:
-                query_user = User.get(username = username)
-                message['error'] = "This username is already exist"
-            except:
-                hash_password = hashlib.sha256(str(password).encode('utf-8')).hexdigest()
-                create_user = User.create(username = username, password = hash_password)
-                create_user.save()
-                return json({"success":"User created successfully."})
-        return json({'error': message})
+            raise exceptions.SanicException("username and password required")
+        try:
+            User.get(User.username ==  username)
+            raise exceptions.SanicException("username already exists")
+        except User.DoesNotExist:
+            hash_password = hashlib.sha256(str(password).encode('utf-8')).hexdigest()
+            user_create = User(username = username, password = hash_password)
+            return json({'success':'User signed up successfully.'})
+
+
+        
 
 
 class TweetList(HTTPMethodView):
     async def get(self,request):
         tweets = []
-        print(request['session']['user'])
         for tweet in Tweet.select():
             tweets.append({'id': tweet.id, 'user': str(tweet.user.username), 'message': tweet.message, 'is_published': tweet.is_published, 'created': tweet.created})
         return json(tweets)
@@ -65,11 +72,12 @@ class TweetList(HTTPMethodView):
     async def post(self, request):
         message = {}
         message = request.json.get('message',None)
-
+        
+        user_id = int(request['session']['user_id'])
         if message is None:
-            message['error'] = "Message is required"
+            raise exceptions.SanicException("message is required")
         else:
-            create_tweet = Tweet.create(user = 1, message = message)
+            create_tweet = Tweet.create(user = user_id, message = message)
             create_tweet.save()
             return json({'success': 'Tweet posted successfully'})
         return json({'error':message})
@@ -88,7 +96,7 @@ class TweetDetails(HTTPMethodView):
         try:
             instance = Tweet.get(id = pk)
         except:
-            return json({'error': 'no record found'})
+            raise exceptions.NotFound("record not found", status_code = 404)
         
         message = request.json.get('message',None)
 
@@ -104,7 +112,9 @@ class TweetDetails(HTTPMethodView):
 
 
 if __name__ == "__main__":
-    app.add_route(UserRegister.as_view(), '/sign-in')
+    app.add_route(UserRegister.as_view(), '/sign-up')
     app.add_route(TweetList.as_view(), '/tweet')
     app.add_route(TweetDetails.as_view(), '/tweet/<pk:int>')
     app.run(debug = True)
+
+
